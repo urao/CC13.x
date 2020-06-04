0208b040f9df6b5771574954815853c39d13ac98
Umamaheshwar <urao@juniper.net>
2020-06-03 18:14:26 -0700

    Modified script

--- utility_scripts/cleanup_script.sh
+++ utility_scripts/cleanup_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-y | /var/lib/contrail_cloud/scripts/install_contrail_command.sh -c
-y | /var/lib/contrail_cloud/scripts/appformix-deploy.sh -c
-y | /var/lib/contrail_cloud/scripts/openstack-post-deploy.sh -c
-y | /var/lib/contrail_cloud/scripts/openstack-deploy.sh -c
-y | /var/lib/contrail_cloud/scripts/storage-nodes-assign.sh -c
-y | /var/lib/contrail_cloud/scripts/compute-nodes-assign.sh -c
-y | /var/lib/contrail_cloud/scripts/control-vms-deploy.sh -c
-y | /var/lib/contrail_cloud/scripts/control-hosts-deploy.sh -c
-y | /var/lib/contrail_cloud/scripts/inventory-assign.sh -c
-N | /var/lib/contrail_cloud/scripts/install_contrail_cloud_manager.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/install_contrail_command.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/appformix-deploy.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/openstack-post-deploy.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/openstack-deploy.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/storage-nodes-assign.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/compute-nodes-assign.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/control-vms-deploy.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/control-hosts-deploy.sh -c
+echo "y" | /var/lib/contrail_cloud/scripts/inventory-assign.sh -c
+echo "N" | /var/lib/contrail_cloud/scripts/install_contrail_cloud_manager.sh -c
